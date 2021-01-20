ba4803a631dd5d65092ae550f8cef91e5b0ef852
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-20 12:44:19 +0530

    Update Data_visualisation.py

--- frozenSpider/Data_visualisation.py
+++ frozenSpider/Data_visualisation.py
@@ -97,7 +97,7 @@ Email - mrfrozenpeak@gmail.com
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-
+import random
 
 
 
@@ -218,7 +218,7 @@ class Logistic_regression_plot():
             ax.scatter3D(class3[0], class3[1], class3[2], color=self.test_class0_color, alpha=self.test_alpha, label=self.test_class0_label)
             ax.scatter3D(class4[0], class4[1], class4[2], color=self.test_class1_color, alpha=self.test_alpha, label=self.test_class1_label)
         if not(save_fig_path=="dont"):
-            plt.savefig(save_fig_path)
+            plt.savefig(save_fig_path + "/3D" + self.title)
 
         plt.show()
 
@@ -353,9 +353,156 @@ class Logistic_regression_plot2D():
         plt.xlabel(self.x_label, fontdict={"fontsize": 15})
         plt.ylabel(self.y_label, fontdict={"fontsize": 15})
         if not(save_fig_path=="dont"):
-            plt.savefig(save_fig_path)
+            plt.savefig(save_fig_path + "/2D" + self.title)
 
         plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Knn_plot():
+
+
+
+    #Init method to set default values..................................................
+
+    def __init__(self, model):
+        self.model = model
+        self.title = "K-nearest neighbours visulisation"
+        self.background_color = (0, 0, 0)
+        self.test_alpha = 0.6
+        self.train_alpha = 0.9
+        self.color_dict = {"Red": "#FF0000", "Orange": "#FFA500", "Yellow": "#FFFF00", "DeepPink": "#FF1493",
+                           "LightPink": "#FFB6C1", "LightPink": "#FFB6C1"
+            , "Pink": "#FFC0CB", "Lavender": "#E6E6FA", "Orchid": "#DA70D6", "Violet": "#EE82EE",
+                           "DarkOrchid": "#9932CC", "DarkViolet": "#9400D3"
+            , "BlueViolet": "#8A2BE2", "Purple": "#800080", "Purple": "#800080", "Indigo": "#4B0082",
+                           "Salmon": "#FA8072", "Crimson": "#DC143C", "DarkRed": "#8B0000",
+                           "DarkOrange": "#FF8C00", "Coral": "#FF7F50", "OrangeRed": "#FF4500", "Gold": "#FFD700",
+                           "GreenYellow": "#ADFF2F", "Lime": "#00FF00", "PaleGreen": "#98FB98"
+            , "SpringGreen": "#00FF7F", "Green": "#008000", "LightSeaGreen": "#20B2AA", "Cyan": "#00FFFF",
+                           "Aquamarine": "#7FFFD4", "SkyBlue": "#87CEEB",
+                           "DeepSkyBlue": "#00BFFF", "Blue": "#0000FF", "MediumBlue": "#0000CD", "Navy": "#000080",
+                           "Black": "#000000", "Gray": "#808080"}
+        self.train_data_color = [key for key in sorted(self.color_dict)[:15]]
+        self.test_data_color = [key for key in sorted(self.color_dict)[15:]]
+        random.shuffle(self.train_data_color)
+        random.shuffle(self.test_data_color)
+
+
+
+
+
+
+    #Method to set the properties of the graph................................
+    #Not necessary, because all the default values are already set......................................
+
+    def set_plot_properties(self, train_alpha = 0.9,test_alpha = 0.6, background_color = (0, 0, 0),title = "K-nearest neighbours visulisation"):
+        self.title = title
+        self.background_color = background_color
+        self.test_alpha = test_alpha
+        self.train_alpha = train_alpha
+
+
+
+
+
+    #method that does all the calculatons before plotting the graph.....................................................
+
+    def plot_3D_calculations(self, model1, x, y):
+        tsne_data = model1.fit_transform(x)
+        classes_dict = {}
+
+        for i in range(len(tsne_data)):
+            if y[i] in classes_dict:
+                classes_dict[y[i]].append(tsne_data[i])
+            else:
+                classes_dict[y[i]] = [tsne_data[i]]
+        return classes_dict
+
+
+
+
+
+
+
+
+    # Method to plot the 2d graph of the calculated values after apllying T-sne.......................................
+
+    def plot_3D_visuals(self, plot_train_data =True, plot_test_data = True, save_fig_path="dont"):
+        model1 = TSNE(n_components=3)
+
+        if plot_train_data:
+             train_classes_dict = self.plot_3D_calculations(model1, self.model.x_data, self.model.y_data)
+        if plot_test_data:
+             test_classes_dict = self.plot_3D_calculations(model1, self.model.x_classified, self.model.y_classified)
+
+        plt.style.use('dark_background')
+
+        fig = plt.figure(figsize=(12, 12))
+        ax = plt.axes(projection='3d')
+        ax.text2D(0.30, 0.98, self.title, transform=ax.transAxes)
+
+        ax.set_facecolor(self.background_color)
+        if plot_train_data:
+            label_dist = 0.02
+            ax.text2D(0.86, 0.98, "Train ", transform=ax.transAxes)
+            for group in train_classes_dict:
+                plotx = []
+                ploty = []
+                plotz = []
+                for coordp in train_classes_dict[group]:
+                    plotx.append(coordp[0])
+                    ploty.append(coordp[1])
+                    plotz.append(coordp[2])
+
+                ax.text2D(0.86, 0.98-label_dist, group, transform=ax.transAxes, color=self.train_data_color[int(label_dist/0.02)%15-1])
+                ax.scatter3D(plotx, ploty, plotz, alpha=self.train_alpha, color=self.train_data_color[int(label_dist/0.02)%15-1])
+                label_dist+=0.02
+        if plot_test_data:
+            label_dist = 0.02
+            ax.text2D(0.98, 0.98, "Test ", transform=ax.transAxes)
+            for group in test_classes_dict:
+                plotx = []
+                ploty = []
+                plotz = []
+                for coordp in test_classes_dict[group]:
+                    plotx.append(coordp[0])
+                    ploty.append(coordp[1])
+                    plotz.append(coordp[2])
+
+                ax.text2D(0.98, 0.98-label_dist, group , transform=ax.transAxes, color=self.test_data_color[int(label_dist/0.02)%15-1])
+                ax.scatter3D(plotx, ploty, plotz, alpha=self.train_alpha, color=self.test_data_color[int(label_dist/0.02)%15-1])
+                label_dist+=0.02
+        if not(save_fig_path=="dont"):
+            plt.savefig(save_fig_path + "/3D" + self.title)
+
+        plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
