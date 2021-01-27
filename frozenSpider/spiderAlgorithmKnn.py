1be26c02f30663817f4e69ae8c9e68adf6d2d75a
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-27 23:17:48 +0530

    Update spiderAlgorithmKnn.py

--- frozenSpider/spiderAlgorithmKnn.py
+++ frozenSpider/spiderAlgorithmKnn.py
@@ -5,7 +5,7 @@ import random
 from sklearn import datasets
 from sklearn.model_selection import  train_test_split
 from frozenSpider import Data_visualisation as dt
-
+from frozenSpider import spiderAlgorithmResources as res
 
 
 class K_nearest_neighbours():
@@ -115,18 +115,7 @@ class plot_model():
         self.ylabel_size = 15
         self.title_size = 18
         self.model = model
-        self.color_dict = {"Red": "#FF0000", "Orange": "#FFA500", "Yellow": "#FFFF00", "DeepPink": "#FF1493",
-                           "LightPink": "#FFB6C1", "LightPink": "#FFB6C1"
-            , "Pink": "#FFC0CB", "Lavender": "#E6E6FA", "Orchid": "#DA70D6", "Violet": "#EE82EE",
-                           "DarkOrchid": "#9932CC", "DarkViolet": "#9400D3"
-            , "BlueViolet": "#8A2BE2", "Purple": "#800080", "Purple": "#800080", "Indigo": "#4B0082",
-                           "Salmon": "#FA8072", "Crimson": "#DC143C", "DarkRed": "#8B0000",
-                           "DarkOrange": "#FF8C00", "Coral": "#FF7F50", "OrangeRed": "#FF4500", "Gold": "#FFD700",
-                           "GreenYellow": "#ADFF2F", "Lime": "#00FF00", "PaleGreen": "#98FB98"
-            , "SpringGreen": "#00FF7F", "Green": "#008000", "LightSeaGreen": "#20B2AA", "Cyan": "#00FFFF",
-                           "Aquamarine": "#7FFFD4", "SkyBlue": "#87CEEB",
-                           "DeepSkyBlue": "#00BFFF", "Blue": "#0000FF", "MediumBlue": "#0000CD", "Navy": "#000080",
-                           "Black": "#000000", "Gray": "#808080"}
+        self.color_dict = res.Resources.get_color_dict()
         self.calculated_point_size = 10
         self.calculated_point_alpha = 0.5
         self.train_labels = []
@@ -358,7 +347,8 @@ pl = plot_model(a)
 
 plot1 = dt.Knn_plot(a)
 plot1.plot_3D_visuals()
-
+plot2 = dt.Knn_plot_2D(a)
+plot2.plot_3D_visuals()
 
 
 
