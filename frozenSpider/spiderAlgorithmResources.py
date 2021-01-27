fa62285057d435ce34e3b1a852b1ae84cfb29776
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-27 23:15:27 +0530

    Add files via upload

---
+++ frozenSpider/spiderAlgorithmResources.py
@@ -0,0 +1,54 @@
+
+
+
+
+class Resources:
+
+
+
+    color_dict = {
+                           "Red": "#FF0000",
+                           "Orange": "#FFA500",
+                           "Yellow": "#FFFF00",
+                           "DeepPink": "#FF1493",
+                           "LightPink": "#FFB6C1",
+                           "Pink": "#FFC0CB",
+                           "Lavender": "#E6E6FA",
+                           "Orchid": "#DA70D6",
+                           "Violet": "#EE82EE",
+                           "DarkOrchid": "#9932CC",
+                           "DarkViolet": "#9400D3",
+                           "BlueViolet": "#8A2BE2",
+                           "Purple": "#800080",
+                           "Indigo": "#4B0082",
+                           "Salmon": "#FA8072",
+                           "Crimson": "#DC143C",
+                           "DarkRed": "#8B0000",
+                           "DarkOrange": "#FF8C00",
+                           "Coral": "#FF7F50",
+                           "OrangeRed": "#FF4500",
+                           "Gold": "#FFD700",
+                           "GreenYellow": "#ADFF2F",
+                           "Lime": "#00FF00",
+                           "PaleGreen": "#98FB98",
+                           "SpringGreen": "#00FF7F",
+                           "Green": "#008000",
+                           "LightSeaGreen": "#20B2AA",
+                           "Cyan": "#00FFFF",
+                           "Aquamarine": "#7FFFD4",
+                           "SkyBlue": "#87CEEB",
+                           "DeepSkyBlue": "#00BFFF",
+                           "Blue": "#0000FF",
+                           "MediumBlue": "#0000CD",
+                           "Navy": "#000080",
+                           "Black": "#000000",
+                           "Gray": "#808080"}
+
+
+    @staticmethod
+    def get_color_dict():
+
+        return Resources.color_dict
+
+
+#print(Resources.get_color_dict())
