05ffdd3b312830df2cbc8ff35c68906521177176
mrfrozenpeak <40189138+mrfrozen97@users.noreply.github.com>
2021-01-27 23:16:38 +0530

    Update spiderAlgorithmLoR.py

--- frozenSpider/spiderAlgorithmLoR.py
+++ frozenSpider/spiderAlgorithmLoR.py
@@ -188,6 +188,10 @@ Email - mrfrozenpeak@gmail.com
 
 import numpy as np
 import matplotlib.pyplot as plt
+from frozenSpider import spiderAlgorithmResources as res
+#from sklearn import datasets
+#from sklearn.model_selection import  train_test_split
+#from frozenSpider import Data_visualisation as dv
 
 
 
@@ -323,12 +327,7 @@ class plot_model(LogisticRegression):
         self.ylabel_size = 15
         self.title_size = 18
         self.model = model
-        self.color_dict = {"Red":"#FF0000", "Orange": "#FFA500", "Yellow": "#FFFF00", "DeepPink" :"#FF1493", "LightPink":"#FFB6C1","LightPink":"#FFB6C1"
-                           ,"Pink":"#FFC0CB", "Lavender":"#E6E6FA", "Orchid":"#DA70D6", "Violet":"#EE82EE", "DarkOrchid": "#9932CC", "DarkViolet":"#9400D3"
-                           ,"BlueViolet":"#8A2BE2", "Purple":"#800080", "Purple":"#800080", "Indigo":"#4B0082", "Salmon":"#FA8072", "Crimson":"#DC143C","DarkRed":"#8B0000",
-                           "DarkOrange":"#FF8C00", "Coral":"#FF7F50","OrangeRed":"#FF4500", "Gold":"#FFD700", "GreenYellow":"#ADFF2F", "Lime":"#00FF00", "PaleGreen":"#98FB98"
-                           ,"SpringGreen":"#00FF7F", "Green":"#008000", "LightSeaGreen":"#20B2AA", "Cyan":"#00FFFF", "Aquamarine":"#7FFFD4", "SkyBlue":"#87CEEB",
-                           "DeepSkyBlue":"#00BFFF", "Blue":"#0000FF", "MediumBlue":"#0000CD", "Navy":"#000080", "Black":"#000000", "Gray":"#808080"}
+        self.color_dict = res.Resources.get_color_dict()
         self.calculated_point_size = 10
         self.calculated_point_class0_label = "Calculated class 0"
         self.calculated_point_class1_label = "Calculated class 1"
@@ -574,7 +573,7 @@ plot.plot_model(save_fig_file="./sample_graphs", display_graph=False)
 
 """
 datav = dv.Logistic_regression_plot(lor)
-datav.plot_3D_visuals(plot_test_data=True)
+datav.plot_3D_visuals(plot_test_data=True, save_fig_path="./sample_graphs")
 """
 
 
@@ -583,7 +582,7 @@ datav.plot_3D_visuals(plot_test_data=True)
 
 """
 datav = dv.Logistic_regression_plot2D(lor)
-datav.plot_2D_visuals(plot_test_data=True)
+datav.plot_2D_visuals(plot_test_data=True, save_fig_path="./sample_graphs")
 
 """
 
